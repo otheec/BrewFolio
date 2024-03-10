641b309ea58e1cd261a62e81fc0c61b01d53fe69
otheec <nezdaril.o@seznam.cz>
2024-03-10 23:39:00 +0100

    brewery page rework
    added:
    -search input
    -filter dropdowns
    -list rework

--- brewfolio.client/src/pages/Breweries.tsx
+++ brewfolio.client/src/pages/Breweries.tsx
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Pagination from '../components/Pagination';
 import { PlaceholderIcon } from '../components/SvgIcons';
+import { SearchIcon } from '../components/SvgIcons';
 
 interface PaginationState {
   breweries: Brewery[];
@@ -47,34 +48,77 @@ const Breweries: React.FC = () => {
   return (
     <>
       <Navbar />
-      <main className="container my-4">
-        <div className="my-3 p-3 bg-body-tertiary rounded shadow-sm">
-          <div className="d-flex border-bottom justify-content-between align-items-center pb-3">
-            <h1 className="mb-0">Breweries</h1>
+      <main className="container mb-4 mt-2">
+        <h1 className="border-bottom">Breweries</h1>
+        <div className="d-none d-sm-flex pb-2 justify-content-between">
+          <div className="d-flex">
+            <div className="dropdown pe-2">
+              <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Status Filter
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </div>
+            <div className="dropdown">
+              <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Type Filter
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="pe-2">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Search..." aria-label="Search" />
+                <span className="input-group-text">
+                  <SearchIcon />
+                </span>
+              </div>
+            </div>
             <Link to={`/brewery/add`} style={{ textDecoration: 'none', color: "#000000" }}>
-              <button className="btn btn-success">+ Add Brewery</button>
+              <button className="btn btn-success text-nowrap">+ Add Brewery</button>
             </Link>
           </div>
-          {pagination.breweries.map((brewery) => (
-            <div key={brewery.id} className="d-flex text-muted pt-3">
-              <PlaceholderIcon />
-              <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
-                <div className="d-flex justify-content-between">
+        </div>
+        <div className="border rounded">
+          <div className="border-bottom p-2 bg-body-secondary  ">
+            <div className="row">
+              <div className="col-3">
+                Name
+              </div>
+              <div className="col-7">
+                Long name
+              </div>
+            </div>
+          </div>
+          {pagination.breweries.map((brewery, index) => (
+            <div key={brewery.id} className={index !== pagination.breweries.length - 1 ? "border-bottom p-2" : "p-2"}>
+              <div className="row">
+                <div className="col-3">
                   <Link to={`/brewery/${brewery.id}`} style={{ textDecoration: 'none', color: "#000000" }}>
-                    <strong>{brewery.name}</strong>
+                    {brewery.name}
                   </Link>
-                  <div className="ms-auto d-flex justify-content-end">
-                    <Link to={`/brewery/edit/${brewery.id}`} style={{ textDecoration: 'none', color: "#000000" }}>
-                      <button type="button" className="page-link pe-3">Edit</button>
-                    </Link>
-                  </div>
                 </div>
-                <span className="d-block">{brewery.longName}</span>
+                <div className="col-7">
+                  <span className="d-block">{brewery.longName}</span>
+                </div>
+                <div className="col-2 d-flex justify-content-end">
+                  <Link to={`/brewery/edit/${brewery.id}`} style={{ textDecoration: 'none', color: "#000000" }}>
+                    <button type="button" className="page-link pe-3">Edit</button>
+                  </Link>
+                </div>
               </div>
             </div>
           ))}
-          <Pagination currentPage={pagination.currentPage} totalPages={Math.ceil(pagination.totalBreweries / pagination.pageSize)} onPageChange={handlePageChange} />
         </div>
+        <Pagination currentPage={pagination.currentPage} totalPages={Math.ceil(pagination.totalBreweries / pagination.pageSize)} onPageChange={handlePageChange} />
       </main>
       <Footer />
     </>
